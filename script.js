69e7803cc6b0861d3f97e0fde9d00446bd24cf98
Billayye <Billayye@hotmail.com>
2022-05-26 00:15:39 +1000

    Create function that asks User for their move

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function generateComputerChoice() {
     let choice = GAME_CHOICES[randIndex];
     return choice;
 }
+
+function getUserChoice() {
+    let isValidChoice = false;
+    let input = "";
+
+    while (!isValidChoice) {
+        input = prompt("Enter your move:", "");
+        input = input.toLowerCase();
+
+        if (input === "rock" || input === "paper" || input === "scissors") {
+            isValidChoice = true;
+        } else {
+            alert("Invalid choice! Try again!");
+        }
+    }
+
+    return input;
+}
